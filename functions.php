4ae2c226a11997da1077d10f41af48c572941e13
Sahil Gulati <sahilgulati49@gmail.com>
2020-09-21 12:26:35 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'salcode_add_plugin_page_settings_link');
+function salcode_add_plugin_page_settings_link( $links ) {
+	$links[] = '<a href="' .
+		admin_url( 'options-general.php?page=nummuspay-integration' ) .
+		'">' . __('Settings') . '</a>';
+	return $links;
+}
